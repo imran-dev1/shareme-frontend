55214b1aa7d8e497fe3439cabfc58b7e7e7d7349
webdev-it <webdevit3@gmail.com>
2022-06-13 03:27:18 +0600

    Added google authentication

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyDwv0WLeBQ-OgnGdOhnSLptP1WA4xCokBo",
-  authDomain: "shareme-ia.firebaseapp.com",
-  projectId: "shareme-ia",
-  storageBucket: "shareme-ia.appspot.com",
-  messagingSenderId: "532241418035",
-  appId: "1:532241418035:web:52c8b731c2c91588cce046"
+   apiKey: "AIzaSyDwv0WLeBQ-OgnGdOhnSLptP1WA4xCokBo",
+   authDomain: "shareme-ia.firebaseapp.com",
+   projectId: "shareme-ia",
+   storageBucket: "shareme-ia.appspot.com",
+   messagingSenderId: "532241418035",
+   appId: "1:532241418035:web:52c8b731c2c91588cce046",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default auth;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,8 +2,32 @@ import React from "react";
 import video from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
 import { FcGoogle } from "react-icons/fc";
+import auth from "../firebase.init";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import { client } from "../components/client";
 
 const Login = () => {
+   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+   const navigate = useNavigate();
+
+   if (loading) {
+      return <h1>Loading...</h1>;
+   }
+   if (user) {
+      const { displayName, photoURL, uid } = user.user;
+
+      const doc = {
+         _id: uid,
+         _type: "user",
+         userName: displayName,
+         image: photoURL,
+      };
+      client.createIfNotExists(doc).then((res) => {
+         navigate("/", { replace: true });
+      });
+   }
+
    return (
       <div className="flex items-center justify-start h-screen flex-col">
          <div className="relative w-full h-full">
@@ -18,7 +42,10 @@ const Login = () => {
             ></video>
             <div className="absolute flex justify-center items-center flex-col left-0 top-0 right-0 bottom-0 bg-blackOverlay/70 gap-5 ">
                <img src={logo} alt="" className=" max-w-[150px]" />
-               <button className="bg-white rounded-md flex items-center p-2 gap-2 outline-none shadow-2xl shadow-black">
+               <button
+                  onClick={() => signInWithGoogle()}
+                  className="bg-white rounded-md flex items-center p-2 gap-2 outline-none shadow-2xl shadow-black"
+               >
                   <FcGoogle className="text-2xl"></FcGoogle> Sign in with Google
                </button>
             </div>
